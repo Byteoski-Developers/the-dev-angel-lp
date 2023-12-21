b0575d3f68821b21546739b0aa449fc33a70970f
Sanyam Ratreja <sanyamratreja18@gmail.com>
2023-12-21 12:21:02 +0530

    push

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../public/assets/images/Logo.svg';
-export default function landingPage() {
+let fun = function landingPage() {
   return (
     <>
-    
+      <p>hkadfkafkjnasdf</p>
     </>
   )
 }
+
+export default fun;
