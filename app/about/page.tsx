430173670da61bf1f7ad09d5d4fe6726d2186aab
lovishbansal00 <lovish.bansal.330@gmail.com>
2023-12-23 17:41:52 +0530

    yarn build done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Footer from "@/components/Footer";
 
 const cardsData = [
     {
@@ -44,7 +45,7 @@ export default function about() {
                 />
                 {
                     cardsData.map((card) => (
-                        <div className='w-full relative flex flex-row'>
+                        <div key={card.date} className='w-full relative flex flex-row' >
                             <div className='flex flex-col mr-1 w-1/12'>
                                 <div className='mt-3 font-poppins text-gray font-medium'>
                                     {card.date}
@@ -61,11 +62,11 @@ export default function about() {
                                     />
                                 </div>
                             </div>
-                            <div className='flex flex-col w-2/3 pb-10 '>
-                                <h4 className="font-semibold mb-1 text-xl ">
+                            <div className='flex flex-col w-2/3 pb-10'>
+                                <h4 className="font-semibold mb-1 text-xl">
                                     {card.blackTitle}<span className="text-orangered"> {card.orangeredTitle}</span>
                                 </h4>
-                                <p className="leading-relaxed  text-xs  ">
+                                <p className="leading-relaxed text-xs">
                                     {card.description}
                                 </p>
                             </div>
@@ -76,15 +77,16 @@ export default function about() {
             <div className='border border-t border-orangered' />
             <div className='mx-30 mt-12 text-base'>
                 <p className='text-center'>
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes from a line in section 1.10.32.
                 </p>
                 <p className='text-center'>
-                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from &quot;de Finibus Bonorum et Malorum&quot; by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
                 </p>
                 <p className='text-center'>
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes from a line in section 1.10.32.
                 </p>
             </div>
+            <Footer />
         </>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Course from "@/components/Course";
+import Footer from "@/components/Footer";
 
 export default function landingPage() {
   return (
     <>
       <Course />
+      <Footer />
     </>
   );
 }
