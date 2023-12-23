45246ab6d22de192995bbbe2025a77424250349f
Gautam Singla <ggsingla777@gmail.com>
2023-12-23 21:58:53 +0530

    Fix footer copy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
             <div className="mx-15">
                 <Image src="/assets/images/Logo.svg" alt="Logo" width={127} height={52} />
             </div>
-            <div className="text-white text-xs py-8 mx-15">Copyright © 2023 All rights reserved</div>
+            <div className="text-white text-xs py-8 mx-15">Copyright &copy; 2023 All rights reserved</div>
         </div>
     </footer>
 }
