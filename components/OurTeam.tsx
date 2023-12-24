f5c3f6d682f58151ad3fadaae12ff950d26eaea9
mehaksingla2005 <124077325+mehaksingla2005@users.noreply.github.com>
2023-12-24 21:12:53 +0530

    WIP

---
+++ components/OurTeam.tsx
@@ -0,0 +1,13 @@
+const OurTeam=()=>{
+    return(<>
+        <div className="font-poppins text-center px-10 md:px-4 lg:px-12 ">
+            <h2 className="text-black font-bold h-12 w-full leaing-12 text-3xl py-10 lg:py-12">&lt; Our <span className="text-orangered">Team !</span> &gt;</h2>
+            <h3 className="text-gray-500 font-medium text-base md:text-xl leading-9 h-9 w-full">Your One Stop Destination for Success</h3>
+        </div>
+        <div>
+            <div><img src="" alt="" className="h-52 w-52 border border-radius-2 " /></div>
+        </div>
+        </>
+    )
+}
+export default OurTeam
---
+++ components/ProductBuilt.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+interface ProductBuiltProps{
+  title:string
+  desp:string 
+  img:string 
+}
+
+const ProductBuilt=({
+  title,
+desp,
+img,
+}:ProductBuiltProps)=> {
+    return (
+        <React.Fragment>
+            
+            
+  <div className="text-center font-poppins px-10 md:px-4 lg:px-12 xl:px-40">
+<h2 className="text-black h-12 w-full font-bold text-3xl leading-12 py-10 lg:py-12" >&lt; Product <span className="text-orangered">to built</span> &gt;</h2>
+<h3 className="text-gray-500 font-medium text-base py-2 md:text-xl leading-9 h-9 w-full">Your One Stop Destination for Success</h3></div>
+
+<div className="items-center justify-center px-36 py-10 ">
+      <div className=" w-[1200px] h-[193px] bg-white rounded-lg border-4 border-solid border-transparent shadow-[0px_10px_20px_#ff57221a] [border-image:linear-gradient(to_bottom,rgb(255,87,34),rgb(255,193,7))_1]" />
+    
+      </div>
+
+</React.Fragment>
+    )
+
+}
+export default ProductBuilt
