706728134ce9aff85cc08534ba8ed4574150ad19
Arushi Gupta <114179213+Arushi-Gupta13@users.noreply.github.com>
2023-12-25 22:34:52 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Image from "next/image";
+
+
+const AboutUs = () => {
+  return (
+    <div className="w-full text-center font-poppins px-10 md:px-5 lg:px-16 xl:px-40">
+      <h2 className="text-3xl py-2 lg:py-4 font-bold ">
+        {"<"} About Us {">"}
+      </h2>
+      <h3 className="text-base md:text-xl leading-9 font-medium text-gray-600">
+        Your One Stop <span className="text-orangered">Destination for Success</span> 
+      </h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:py-5">
+        <div className="text-center md:text-left md:col-span-1 md:py-7 lg:py-14">
+          <h4 className="text-xl md:text-2xl leading-9 font-semibold py-2">
+            Explore <span className="text-orangered">Our Course :</span>
+          </h4>
+          <p className="py-3 lg:py-5 font-normal text-base leading-6">
+            Unlock your potential with our diverse range of courses designed to
+            meet your learning needs. Whether you&apos;re a beginner or an expert
+            looking to enhance your skills, our carefully curated selection
+            offers something for everyone. Dive into a world of knowledge and
+            opportunity with:
+          </p>
+          <button className="bg-orangered px-4 gap-2.5 py-2 w-80 h-10 rounded-lg">
+            <div className=" text-white font-inter text-base leading-5">
+              Explore Our Courses {"-->"}
+            </div>
+          </button>
+        </div>
+        <div className="md:col-span-1 text-center ">
+          <Image
+            src={"/assets/images/Couse.svg"}
+            width={410}
+            height={303}
+            alt="About Us"
+            className="inline"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
