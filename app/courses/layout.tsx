c929394e3b4135d300505e963222e27080f543f7
prashant <prash2402@gmail.com>
2024-01-04 00:27:25 +0530

    Fix: created course layout and UI improvements

---
+++ app/courses/layout.tsx
@@ -0,0 +1,18 @@
+import CourseMenu from "@/components/Courses/CourseMenu";
+import ContactCTA from "@/components/common/ContactCTA";
+
+export default function CourseLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="my-12 view-container">
+      {/* header */}
+      <div className="flex flex-col gap-2">
+        <CourseMenu />
+        {children}
+        <ContactCTA />
+      </div>
+    </div>)
+}
--- components/Courses/CourseMenu.tsx
+++ components/Courses/CourseMenu.tsx
@@ -1,33 +1,38 @@
+"use client";
+import { usePathname } from 'next/navigation'
+
 const MenuItems = [
-  { label: "Fontend", url: "/courses/fullstack" },
-  { label: "Backend", url: "/courses/fullstack" },
-  { label: "Database", url: "/courses/fullstack" },
-  { label: "FullStack", url: "/courses/fullstack" },
-  { label: "UI/UX", url: "/courses/fullstack" },
+  { label: "Frontend", url: "/courses/frontend" },
+  { label: "Backend", url: "/courses/backend" },
+  { label: "Database", url: "/courses/database" },
+  { label: "FullStack", url: "/courses" },
+  { label: "UI/UX", url: "/courses/uiux" },
 ];
 
 export default function CourseMenu() {
+  const pathname = usePathname()
   return (
-    <div className="bg-gradient-to-r from-primaryOrange p-[2px] to-secondaryYellow rounded-lg">
-      <div className="md:text-base w-full bg-white text-sm flex flex-wrap  px-6 py-4 md:gap-0 gap-2  rounded-md md:overflow-hidden">
-        {MenuItems.map((menuItem, index) => (
-          <a
-            key={index}
-            href={menuItem.url}
-            className="text-black hover:text-gray-500"
-          >
-            <span
-              className={`border-r ${
-                index === MenuItems.length - 1
+    <>
+      <p>Select Course*</p>
+      <div className="bg-gradient-to-r from-primaryOrange p-[2px] to-secondaryYellow rounded-lg">
+        <div className="md:text-base w-full bg-white text-sm flex flex-wrap px-6 py-4 md:gap-0 gap-2 rounded-md md:overflow-hidden">
+          {MenuItems.map((menuItem, index) => (
+            <a
+              key={index}
+              href={menuItem.url}
+            >
+              <span
+                className={`border-r ${menuItem.url === pathname
                   ? "text-primaryOrange font-bold"
                   : ""
-              } md:pr-5 pr-2 md:mr-4 mr-2 border-black cursor-pointer`}
-            >
-              {menuItem.label}
-            </span>
-          </a>
-        ))}
+                  } md:pr-5 pr-2 md:mr-4 mr-2 border-black cursor-pointer`}
+              >
+                {menuItem.label}
+              </span>
+            </a>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
