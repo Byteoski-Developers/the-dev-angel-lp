660e2a27cb76f1fd9fcc5baf79d1b0e6455ea9e3
mehaksingla2005 <124077325+mehaksingla2005@users.noreply.github.com>
2024-01-05 20:33:47 +0530

    Add files via upload

---
+++ ProductBuilt.tsx
@@ -0,0 +1,19 @@
+const ProductBuilt = () => {
+  return (
+    <>
+      <div className="text-center font-poppins px-10 md:px-4 lg:px-12 xl:px-40">
+        <h2 className="text-black h-12 w-full font-bold text-3xl leading-12 py-10 lg:py-12">
+          &lt; Product <span className="text-orangered">to built</span> &gt;
+        </h2>
+        <h3 className="text-gray-500 font-medium text-base py-2 md:text-xl leading-9 h-9 w-full">
+          Your One Stop Destination for Success
+        </h3>
+      </div>
+
+      <div className="items-center justify-center px-36 py-10 ">
+        <div className=" w-[1200px] h-[193px] bg-white rounded-lg border-4 border-solid border-transparent shadow-[0px_10px_20px_#ff57221a] [border-image:linear-gradient(to_bottom,rgb(255,87,34),rgb(255,193,7))_1]" />
+      </div>
+    </>
+  );
+};
+export default ProductBuilt;
---
+++ page.tsx
@@ -0,0 +1,15 @@
+import Course from "@/components/Course";
+import ProductBuilt from "@/components/ProductBuilt";
+import AboutUs from "@/components/AboutUs";
+import OurTeam from "@/components/OurTeam";
+
+export default function landingPage() {
+  return (
+    <>
+      <Course />
+      <ProductBuilt />
+      <AboutUs />
+      <OurTeam />
+    </>
+  );
+}
