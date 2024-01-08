fa89ae3b6bafe36f224523a02feafb3ef9737bae
mehaksingla <singlamehak2005@gmail.com>
2024-01-08 14:00:45 +0530

    made the site responsive

--- components/ProductBuilt.tsx
+++ components/ProductBuilt.tsx
@@ -1,19 +1,19 @@
 const ProductBuilt = () => {
-    return (
-      <>
-        <div className="text-center font-poppins px-10 md:px-4 lg:px-12 xl:px-40">
-          <h2 className="text-black h-12 w-full font-bold text-3xl leading-12 py-10 lg:py-12">
-            &lt; Product <span className="text-orangered">to built</span> &gt;
-          </h2>
-          <h3 className="text-gray-500 font-medium text-base py-2 md:text-xl leading-9 h-9 w-full">
-            Your One Stop Destination for Success
-          </h3>
-        </div>
-  
-        <div className="items-center justify-center px-36 py-10 ">
-          <div className=" w-[1200px] h-[193px] bg-white rounded-lg border-4 border-solid border-transparent shadow-[0px_10px_20px_#ff57221a] [border-image:linear-gradient(to_bottom,rgb(255,87,34),rgb(255,193,7))_1]" />
-        </div>
-      </>
-    );
-  };
-  export default ProductBuilt;
+  return (
+    <>
+      <div className="text-center font-poppins  w-full px-6 md:px-10 lg:px-12 xl:px-20 2xl:px-40">
+        <h2 className="text-black h-12 w-full font-bold text-3xl leading-12 py-6 md:py-10 lg:py-12">
+          &lt; Product <span className="text-orangered">to built</span> &gt;
+        </h2>
+        <h3 className="text-gray-500 font-medium text-base mt-15 py-2 md:text-xl leading-9 h-9 w-full">
+          Your One Stop Destination for Success
+        </h3>
+      </div>
+
+      <div className="flex items-center justify-center px-6 md:px-10 lg:px-12 xl:px-20 2xl:px-40 py-6 md:py-10 ">
+        <div className=" max-width-full md:max-w-screen-md lg:max-w-screen-lg xl:max-width-screen-xl 2xl:max-width-screen-2xl w-full md:w-[80%] lg:w-[70%] xl:w-[90%] 2xl:w-[70%] h-[150px] md:h-[200px] lg:h-[250px] bg-white rounded-lg border-4 border-solid border-transparent shadow-sm md:shadow-lg [border-image:linear-gradient(to_bottom,rgb(255,87,34),rgb(255,193,7))_1]" />
+      </div>
+    </>
+  );
+};
+export default ProductBuilt;
