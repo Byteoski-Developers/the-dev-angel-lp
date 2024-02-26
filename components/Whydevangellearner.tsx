5b86f88ab889ccd7a2a2f2b8fdc9be72ecdec8f0
viperist <somnathc276@gmail.com>
2024-02-27 00:37:49 +0530

    ....

--- components/Whydevangellearner.tsx
+++ components/Whydevangellearner.tsx
@@ -12,7 +12,7 @@ const Whydevangellearner = () => {
       h2: "Ready Candidates",
       description:
         "The DevAngel is started for supporting students / young Professionals to build their products, learn technology in all domains be it web, apps, devOps, product management, indie Hacking etc.",
-        image: "/assets/images/undraw_experts.png",
+      image: "/assets/images/undraw_experts.png",
     },
     {
       h1: "Diverse",
@@ -26,15 +26,15 @@ const Whydevangellearner = () => {
       h2: "Global Experts",
       description:
         "The DevAngel is started for supporting students / young Professionals to build their products, learn technology in all domains be it web, apps, devOps, product management, indie Hacking etc.",
-        image:"/assets/image/globe.png",
+      image: "/assets/images/globe.png",
     },
-    {
-      h1: "Trusted",
-      h2: "By Industry Leaders",
-      description:
-        "The DevAngel is started for supporting students / young Professionals to build their products, learn technology in all domains be it web, apps, devOps, product management, indie Hacking etc.",
-      image: "/assets/images/undraw_powerful_re_frhr 1.png",
-    },
+    // {
+    //   h1: "Trusted",
+    //   h2: "By Industry Leaders",
+    //   description:
+    //     "The DevAngel is started for supporting students / young Professionals to build their products, learn technology in all domains be it web, apps, devOps, product management, indie Hacking etc.",
+    //   image: "/assets/images/undraw_powerful_re_frhr 1.png",
+    // },
   ];
   return (
     <div className="flex w-full place-content-center">
@@ -62,31 +62,65 @@ const Whydevangellearner = () => {
             >
               <div className="image-container">
                 <Image
-                src={hirecardProps[index].image}
-                alt="....."
-                width={400}
-                height={70}
-                className="hidden sm:flex  max-w-[354px]"
-              ></Image> </div>
+                  src={hirecardProps[index].image}
+                  alt="....."
+                  width={150}
+                  height={35}
+                  className="hidden sm:flex  max-w-[354px]"
+                ></Image>{" "}
+              </div>
 
-                
-
-              <HireCard key={index} {...props} /> 
+              <HireCard key={index} {...props} />
 
               <div
                 className={classNames(
-                  "border-t border-t-1 border-t-black  w-96 h-32 ",
+                  "border-t border-t-1 border-t-black  w-96 h-32  ",
                   index % 2 == 0
                     ? "border-r border-r-1 border-r-black"
                     : "border-l border-l-1 border-l-black"
                 )}
               ></div>
+              <div className="w-[10px] h-[10px] rounded-full bg-orange-500"></div>
             </div>
           ))}
-          <div className = "flex justify-center items-center ">
+          <div>
+            <div
+              className="flex w-screen bg-yellow-100"
+              style={{
+                backgroundColor: "#ecf1e9",
+                backgroundImage:
+                  "linear-gradient(45deg, #ecf1e9 0%, #e5f98c 100%)",
+              }}
+            >
+              <div className="w-11/12  flex justify-end">
+                <div className="flex  justify-center w-1/2 mr-20 ">
+                  <div className="flex flex-col">
+                    <div className="text-xl font-semibold pb-2 font-poppins">
+                      <span className="text-primaryOrange">Trusted {"  "}</span>
+                      <span className="">By Industry Leaders</span>
+                    </div>
+                    <div className="text-sm font-poppins font-medium">
+                      "The DevAngel is started for supporting students / young
+                      Professionals to build their products, learn technology in
+                      all domains be it web, apps, devOps, product management,
+                      indie Hacking etc."
+                    </div>
+                  </div>
+                  <img
+                    src="/assets/images/undraw_powerful_re_frhr 1.png"
+                    alt="Hire Card Iamge"
+                  />
+                  <div className="pl-4 sm:pl-6 lg:pl-8 mt-4">
+                    <span className=""></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-10 flex justify-center items-center ">
             <button className="flex justify-center items-center bg-orange-500 hover:bg-orange-600 text-white font-medium py-1 px-3 rounded">
-                        <span className ="mr-2">Start Hiring</span>
-                        <span className="text-xl font-thin">&gt;</span>
+              <span className="mr-2">Start Hiring</span>
+              <span className="text-xl font-thin">&gt;</span>
             </button>
           </div>
         </div>
