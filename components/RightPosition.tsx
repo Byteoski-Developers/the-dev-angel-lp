614e84cddb57fc5918c5a5fdbc5f6d6525ac9214
viperist <somnathc276@gmail.com>
2024-02-29 12:22:50 +0530

    created an array of data

--- components/RightPosition.tsx
+++ components/RightPosition.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
+import FirstRowSkills from "./FirstRowSkills";
+import SecondRowSkills from "./SecondRowSkills";
+import ThirdRowSkills from "./ThirdRowSkills";
 
 export default function RightPosition() {
   return (
@@ -23,83 +26,19 @@ export default function RightPosition() {
           </button>
         </div>
       </div>
-      <div className=" mt-10 bg-white-100 flex items-center justify-center gap-3">
-        <div className="  bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Coding
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Java
-        </div>
-        <div className=" bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          C++
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Node Js
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Tailwind Css
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Github
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Mobile Development
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Full Stack Developer
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Software Engineer
-        </div>
-      </div>
+      <FirstRowSkills />
 
-      <div className=" mt-6 bg-white-100 flex items-center justify-center gap-3">
-        <div className="  bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Coding
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Java
-        </div>
-        <div className=" bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          C++
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Node Js
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Tailwind Css
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Github
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Mobile Development
-        </div>
-      </div>
+      <SecondRowSkills />
 
-      <div className=" mt-6 bg-white-100 flex items-center justify-center gap-3">
-        <div className="  bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Coding
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Java
-        </div>
-        <div className=" bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          C++
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Node Js
-        </div>
-        <div className="bg-orange-100 p-1 rounded-lg text-orange-500 font-thin text-center">
-          Tailwind Css
-        </div>
-        </div>
-        <div className = "mt-6 flex justify-center items-center ">
-            <button className="flex justify-center items-center bg-orange-500 hover:bg-orange-600 text-white font-medium py-1 px-3 rounded">
-                        <span className ="mr-2">Start Hiring</span>
-                        <span className="text-xl font-thin">&gt;</span>
-            </button>
-            </div>
+      <ThirdRowSkills/>
+
+      
+      <div className="mt-6 flex justify-center items-center ">
+        <button className="flex justify-center items-center bg-orange-500 hover:bg-orange-600 text-white font-medium py-1 px-3 rounded">
+          <span className="mr-2">Start Hiring</span>
+          <span className="text-xl font-thin">&gt;</span>
+        </button>
+      </div>
     </div>
   );
 }
